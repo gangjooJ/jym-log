a0972fd0927dbfb183ca307b0b6ecb5ee682ac48
gangjooJ <jkj960109@gmail.com>
2026-07-13 12:59:08 +0900

    refactor: 앱 설정 파일 분리

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window.JYMLog = window.JYMLog || {};
+
+window.JYMLog.config = Object.freeze({
+  appName: "JYM Log",
+  version: "v0.1.1",
+  updatedAt: "2026-07-13",
+  storageKey: "jym-log-prototype-state",
+  locale: "ko-KR",
+  timezone: "Asia/Seoul"
+});
